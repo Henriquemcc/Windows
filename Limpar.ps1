1f3646b26933f6785fe1be2bc68223ca4918c968
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-05 10:11:28 -0300

    Adicionando a opção de baixar, instalar e executar o Ccleaner

--- Limpar.ps1
+++ Limpar.ps1
@@ -1,11 +1,12 @@
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 2) {
+    while ($_opcao -lt 0 -or $_opcao -gt 3) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Remover arquivos inuteis com o cleanmgr"
         Write-Host -Object "2 - Remover arquivos temporarios"
+        Write-Host -Object "3 - Baixar, instalar e executar o Ccleaner"
         $_opcao = ./Read-Int32.ps1
     }
 
@@ -22,5 +23,9 @@ while ($opcao -ne 0) {
 
     elseif ($opcao -eq 2) {
         .\Remove-TemporaryFiles.ps1
+    }
+
+    elseif ($opcao -eq 3) {
+        .\Start-Ccleaner.ps1
     }
 }
