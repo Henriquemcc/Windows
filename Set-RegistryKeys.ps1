d9e8b65fb7cc1be7093c181f89341db7707ca226
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-05 07:57:24 -0300

    Disabling the command to set Windows Time Zone to dual boot with Linux and adding the command to change startup delay time

--- Set-RegistryKeys.ps1
+++ Set-RegistryKeys.ps1
@@ -2,8 +2,11 @@
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 1 -Force
 
 # Setting Windows Time Zone to dual boot with Linux
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Value 1 -Force
+# Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Value 1 -Force
 
-# Enableing retpoline spectre mitigation
+# Enabling retpoline spectre mitigation
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverride" -Value 0x400 -Force
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverrideMask" -Value 0x400 -Force
+
+# Changing startup delay time
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "StartupDelayInMSec" -Value 0x000 -Force
