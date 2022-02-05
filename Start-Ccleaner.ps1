cbac7bdd83a3bb3c87a15fb2876a1d974a75e706
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-05 10:10:58 -0300

    Adding script

---
+++ Start-Ccleaner.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Runs Ccleaner.
+.DESCRIPTION
+    Runs Ccleaner.
+#>
+
+# Installing Ccleaner if it is not installed
+.\Install-Ccleaner.ps1
+
+# Running Ccleaner
+$process = [System.Diagnostics.Process]::new()
+$process.StartInfo.FileName = [System.IO.Path]::Combine($env:ProgramFiles, "CCleaner", "CCleaner64.exe")
+$process.Start()
+$process.WaitForExit()
