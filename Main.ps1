1c680ce01d31dc9b0f2271358e38870deb42835e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-05 09:55:47 -0300

    Adicionando menu Limpar

--- Main.ps1
+++ Main.ps1
@@ -1,11 +1,12 @@
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 2) {
+    while ($_opcao -lt 0 -or $_opcao -gt 3) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Executar instalacao padrao"
         Write-Host -Object "2 - Reparar o Windows"
+        Write-Host -Object "3 - Limpar o sistema"
         $_opcao = ./Read-Int32.ps1
     }
 
@@ -22,5 +23,9 @@ while ($opcao -ne 0) {
 
     elseif ($opcao -eq 2) {
         .\Reparar.ps1
+    }
+
+    elseif ($opcao -eq 3) {
+        .\Limpar.ps1
     }
 }
