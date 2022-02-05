a22d028cb2ee308a07cbb3664e523c15cfbf50c5
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-05 10:40:04 -0300

    Adding script

---
+++ Start-EsetOnlineScanner.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+    Runs ESET Online Scanner
+.DESCRIPTION
+    Retrieves and execute ESET Online Scanner
+#>
+
+# Downloading ESET Online Scanner
+$url = "https://download.eset.com/com/eset/tools/online_scanner/latest/esetonlinescanner.exe"
+$downloadFile = [System.IO.Path]::Combine($env:TEMP, "esetonlinescanner.exe")
+Invoke-WebRequest -Uri $url -OutFile $downloadFile
+
+# Running ESET Online Scanner
+$process = [System.Diagnostics.Process]::new()
+$process.StartInfo.FileName = $downloadFile
+$process.StartInfo.UseShellExecute = $true
+$process.Start()
+$process.WaitForExit()
