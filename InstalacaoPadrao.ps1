3867da5f615d356b8bd59166e364704fa0a4ddec
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 00:48:08 -0300

    Adicionando script

---
+++ InstalacaoPadrao.ps1
@@ -0,0 +1,23 @@
+function ObterOpcao {
+
+    $_opcao = -1
+    while ($_opcao -lt 0 -or $_opcao -gt 1) {
+        Write-Host -Object "O que deseja fazer?"
+        Write-Host -Object "0 - Sair"
+        Write-Host -Object "1 - Instalar programas Henrique"
+        $_opcao = ./Read-Int32.ps1
+    }
+
+    return $_opcao
+}
+
+$opcao = -1
+while ($opcao -ne 0) {
+    $opcao = ObterOpcao
+
+    if ($opcao -eq 1) {
+        .\InstalacaoHenrique.ps1
+    }
+
+    
+}
