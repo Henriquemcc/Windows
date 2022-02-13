87fccc300e5e5c7d1201c567458dff42fe9505d5
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 11:43:08 -0300

    Formatting script and adding comment

--- Enable-PasswordOnWindowsUAC.ps1
+++ Enable-PasswordOnWindowsUAC.ps1
@@ -1,6 +1,7 @@
-param(
-    
+param(   
     [Parameter(Mandatory = $false)] [System.Management.Automation.SwitchParameter]$Reverse
 )
+
+# https://superuser.com/questions/1085680/windows-10-make-uac-always-require-password
 
 .\Set-RegistryKey.ps1 -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Reverse:$Reverse -Value 1
