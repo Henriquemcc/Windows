92d4d1df221e7d1ff7677716c508f53924efa5ac
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 17:36:43 -0300

    Adicionando opção para configurar open ssh server

--- Configurar.ps1
+++ Configurar.ps1
@@ -1,10 +1,11 @@
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 1) {
+    while ($_opcao -lt 0 -or $_opcao -gt 2) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Configurar registro do sistema"
+        Write-Host -Object "2 - Configurar Open SSH Server"
         $_opcao = ./Read-Int32.ps1
     }
 
@@ -17,5 +18,9 @@ while ($opcao -ne 0) {
 
     if ($opcao -eq 1) {
         .\ConfigurarRegistro.ps1
+    }
+
+    elseif ($opcao -eq 2) {
+        .\ConfigurarOpenSshServer.ps1
     }
 }
