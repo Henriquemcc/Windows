16e46cadb3c3d8e2ec87fe1fddb74d7fca7d1b1f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 13:37:50 -0300

    Adicionando opção para reinstalar bloatware

--- Instalar.ps1
+++ Instalar.ps1
@@ -1,12 +1,13 @@
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 3) {
+    while ($_opcao -lt 0 -or $_opcao -gt 4) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Instalar instalacao padrao"
         Write-Host -Object "2 - Instalar Chocolatey (gerenciador de pacotes)"
         Write-Host -Object "3 - Instalar Ccleaner (utilitario de limpeza)"
+        Write-Host -Object "4 - Instalar/Reinstalar Bloatware"
         $_opcao = ./Read-Int32.ps1
     }
 
@@ -27,5 +28,9 @@ while ($opcao -ne 0) {
 
     elseif ($opcao -eq 3) {
         .\Install-Ccleaner.ps1
+    }
+
+    elseif ($opcao -eq 4) {
+        .\ReinstalarBloatware.ps1
     }
 }
