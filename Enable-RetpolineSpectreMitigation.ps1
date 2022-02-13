cb31ca3098fff21dffff9a3126627a45e9a215db
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 11:48:07 -0300

    Adding reverse option

--- Enable-RetpolineSpectreMitigation.ps1
+++ Enable-RetpolineSpectreMitigation.ps1
@@ -4,5 +4,5 @@ param(
 
 # https://www.bleepingcomputer.com/news/security/boost-windows-10-performance-with-retpoline-spectre-mitigation/
 
-.\Set-RegistryKey.ps1 -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverride" -Value 0x400
-.\Set-RegistryKey.ps1 -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverrideMask" -Value 0x400
+.\Set-RegistryKey.ps1 -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverride" -Value 0x400 -Reverse:$Reverse
+.\Set-RegistryKey.ps1 -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverrideMask" -Value 0x400 -Reverse:$Reverse
