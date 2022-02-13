54ed28b5a414f65a33bed30d597daad154658a98
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-13 00:49:02 -0300

    Adicionando script

---
+++ Instalar.ps1
@@ -0,0 +1,31 @@
+function ObterOpcao {
+
+    $_opcao = -1
+    while ($_opcao -lt 0 -or $_opcao -gt 3) {
+        Write-Host -Object "O que deseja fazer?"
+        Write-Host -Object "0 - Sair"
+        Write-Host -Object "1 - Instalar instalacao padrao"
+        Write-Host -Object "2 - Instalar Chocolatey (gerenciador de pacotes)"
+        Write-Host -Object "3 - Instalar Ccleaner (utilitario de limpeza)"
+        $_opcao = ./Read-Int32.ps1
+    }
+
+    return $_opcao
+}
+
+$opcao = -1
+while ($opcao -ne 0) {
+    $opcao = ObterOpcao
+
+    if ($opcao -eq 1) {
+        .\InstalacaoPadrao.ps1
+    }
+
+    elseif ($opcao -eq 2) {
+        .\Install-Chocolatey.ps1
+    }
+
+    elseif ($opcao -eq 3) {
+        .\Install-Ccleaner.ps1
+    }
+}
