51c0febeca513afafa1e17313fd93f053cdea477
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-15 12:48:22 -0300

    Adding script

---
+++ Uninstall-OpenSshServer.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+    Uninstalls Open SSH Server.
+.DESCRIPTION
+    Uninstalls Open SSH Server.
+#>
+
+# Stopping and removing sshd service
+Stop-Service -Name "sshd" -Force
+Remove-Service -Name "sshd" -Force
+
+# Removing Firewall rule
+if (Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled) {
+    Remove-NetFirewallRule -Name "OpenSSH-Server-In-TCP"
+}
+
+# Removing Open SSH Server
+Remove-WindowsCapability -Online -Name "OpenSSH.Server*"
