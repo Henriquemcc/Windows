41a665516566a99a462ff76f5f60a6e1cc83b41a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-23 21:38:15 -0300

    Alterando o local onde será baixado e extraído o arquivo zip do repositório do GitHub

--- Iniciar.ps1
+++ Iniciar.ps1
@@ -1,8 +1,8 @@
 $url = "https://github.com/Henriquemcc/Windows/archive/refs/heads/main.zip"
 $downloadFileName = "Windows-main.zip"
-$downloadFilePath = [System.IO.Path]::Combine($env:TMP, $downloadFileName)
+$downloadFilePath = [System.IO.Path]::Combine($env:APPDATA, $downloadFileName)
 Invoke-WebRequest -Uri $url -OutFile $downloadFilePath
-Expand-Archive -Path $downloadFilePath -DestinationPath $env:TMP -Force
-$directory = [System.IO.Path]::Combine($env:TMP, $downloadFileName.Substring(0, $downloadFileName.Length - ".zip".Length))
+Expand-Archive -Path $downloadFilePath -DestinationPath $env:APPDATA -Force
+$directory = [System.IO.Path]::Combine($env:APPDATA, $downloadFileName.Substring(0, $downloadFileName.Length - ".zip".Length))
 Set-Location -Path $directory
 .\Main.ps1
