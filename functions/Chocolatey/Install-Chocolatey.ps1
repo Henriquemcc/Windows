f02dc1fafe1362ca2faa905263b6f1ec36bcd072
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-04-24 14:53:57 -0300

    Removing exitOnRebootDetected from chocolatey installation script

--- functions/Chocolatey/Install-Chocolatey.ps1
+++ functions/Chocolatey/Install-Chocolatey.ps1
@@ -15,8 +15,5 @@ function Install-Chocolatey
         # Installing Chocolatey
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-        # Setting up Chocolatey
-        choco feature enable -name=exitOnRebootDetected
     }
 }
