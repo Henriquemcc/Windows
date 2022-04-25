33768f6bab13b717a86a6860562fecdd9ac1315c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-04-25 15:44:20 -0300

    Adicionando opção para atualizar todos os pacotes Chocolatey

--- Atualizar.ps1
+++ Atualizar.ps1
@@ -1,12 +1,15 @@
 ﻿Import-Module -Name ([System.IO.Path]::Combine((Split-Path -Path $MyInvocation.MyCommand.Definition -Parent), "functions", "MyIO", "MyIO.psm1"))
 
-function ObterOpcao {
+function ObterOpcao
+{
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 1) {
+    while ($_opcao -lt 0 -or $_opcao -gt 2)
+    {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Atualizar ajuda do PowerShell"
+        Write-Host -Object "2 - Atualizar todos os pacotes Chocolatey"
         $_opcao = Read-Int32
     }
 
@@ -14,12 +17,18 @@ function ObterOpcao {
 }
 
 $opcao = -1
-while ($opcao -ne 0) {
+while ($opcao -ne 0)
+{
     $opcao = ObterOpcao
 
-    if ($opcao -eq 1) {
+    if ($opcao -eq 1)
+    {
         Update-Help -Force -ErrorAction Ignore
     }
 
-    
+    if ($opcao -eq 2)
+    {
+        Import-Module -Name ([System.IO.Path]::Combine((Split-Path -Path $MyInvocation.MyCommand.Definition -Parent), "functions", "Chocolatey", "Chocolatey.psm1"))
+        Upgrade-ChocolateyPackage -ConfirmAllPrompts
+    }
 }
