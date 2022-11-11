40310a4cd54b89f9398dc44414e87b160b76ce5c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-11-11 09:38:24 -0300

    Fixing Import-Module.

--- functions/CleanWindows/Start-Ccleaner.ps1
+++ functions/CleanWindows/Start-Ccleaner.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module -Name ([System.IO.Path]::Combine((Split-Path -Path $MyInvocation.MyCommand.Definition -Parent), "Install-Ccleaner.ps1"))
+﻿Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition), "Install-Ccleaner.ps1")) -Global
 
 function Start-Ccleaner
 {
