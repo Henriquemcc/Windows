e6d2fcc9aa10ca37f420b27f8bd4ab92aa88b9e1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-11-11 08:56:22 -0300

    Movendo script parar para dentro da pasta Menu.

--- Main.ps1
+++ Main.ps1
@@ -49,7 +49,7 @@ while ($opcao -ne 0) {
     }
 
     elseif ($opcao -eq 7) {
-        .\Parar.ps1
+        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition), "Menu", "Parar.ps1"))
     }
 
     elseif ($opcao -eq 8) {
