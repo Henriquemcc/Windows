21610923d4f13f65506bb873c1e202020eae3ef8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-11-21 16:16:54 -0300

    Adicionando a instalação do Python.

--- Menu/PortableApps/Principal.ps1
+++ Menu/PortableApps/Principal.ps1
@@ -3,7 +3,7 @@ Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryNam
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 13) {
+    while ($_opcao -lt 0 -or $_opcao -gt 14) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
         Write-Host -Object "1 - Instalar Chocolatey"
@@ -18,7 +18,8 @@ function ObterOpcao {
         Write-Host -Object "10 - Instalar LibreOffice"
         Write-Host -Object "11 - Instalar Nomacs"
         Write-Host -Object "12 - Instalar Peazip"
-        Write-Host -Object "13 - Instalar Visual Studio Code"
+        Write-Host -Object "13 - Instalar Python"
+        Write-Host -Object "14 - Instalar Visual Studio Code"
         
         $_opcao = Read-Int32
     }
@@ -79,6 +80,10 @@ while ($opcao -ne 0) {
     }
 
     elseif ($opcao -eq 13) {
+        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))), "PortableApps", "Install-Python.ps1"))
+    }
+
+    elseif ($opcao -eq 14) {
         & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))), "PortableApps", "Install-VisualStudioCode.ps1"))
     }
 }
