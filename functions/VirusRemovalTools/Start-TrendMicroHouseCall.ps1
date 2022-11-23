6586c9524979ae6f2468356e0fd394df876824e7
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-11-23 09:31:51 -0300

    -Changing how the URL is chosen. -Changing how the download output file name is chosen.

--- functions/VirusRemovalTools/Start-TrendMicroHouseCall.ps1
+++ functions/VirusRemovalTools/Start-TrendMicroHouseCall.ps1
@@ -1,5 +1,4 @@
-﻿function Start-TrendMicroHouseCall
-{
+﻿function Start-TrendMicroHouseCall {
     <#
     .SYNOPSIS
         Runs Trend Micro HouseCall.
@@ -7,23 +6,17 @@
         Retrieves and executes Trend Micro HouseCall.
     #>
 
-    $url = ""
-    $downloadFile = ""
-
-    # Setting url and download path
-    if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64")
-    {
-        $url = "https://go.trendmicro.com/housecall8/r2/HousecallLauncher64.exe"
-        $downloadFile = [System.IO.Path]::Combine($env:TEMP, "HousecallLauncher64.exe")
+    # Download Variables
+    $url = if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
+        "https://go.trendmicro.com/housecall8/r2/HousecallLauncher64.exe"
     }
-    elseif ($env:PROCESSOR_ARCHITECTURE -eq "x86")
-    {
-        $url = "https://go.trendmicro.com/housecall8/r2/HousecallLauncher.exe"
-        $downloadFile = [System.IO.Path]::Combine($env:TEMP, "HousecallLauncher.exe")
+    elseif ($env:PROCESSOR_ARCHITECTURE -eq "x86") {
+        "https://go.trendmicro.com/housecall8/r2/HousecallLauncher.exe"
     }
+    $downloadFile = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($url))
 
     # Downloading Trend Micro HouseCall
-    Invoke-WebRequest -Uri $url -OutFile $downloadFile
+    Invoke-WebRequest -Uri:$url -OutFile:$downloadFile
 
     # Running Trend Micro HouseCall
     $process = [System.Diagnostics.Process]::new()
