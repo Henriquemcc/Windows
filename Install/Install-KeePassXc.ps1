b0157f6b6f66bd4b2184f326292cf81ad3665dcb
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-02-06 10:38:39 -0300

    Moving KeePassXc installation script to 'Install' folder and making it universal for installing it as admin and non-admin.

---
+++ Install/Install-KeePassXc.ps1
@@ -0,0 +1,69 @@
+﻿Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "functions", "Util", "Test-AdministratorPrivileges.ps1"))
+
+if (Test-AdministratorPrivileges) {
+    # Download Variables
+    $url = if ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "amd64") {
+        "https://github.com/keepassxreboot/keepassxc/releases/download/2.7.4/KeePassXC-2.7.4-Win64.msi"
+    }
+    elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "x86") {
+        "https://github.com/keepassxreboot/keepassxc/releases/download/2.6.6/KeePassXC-2.6.6-Win32.msi"
+    }
+    else {
+        throw "Invalid Architecture"
+    }
+    $downloadFileName = [System.IO.Path]::GetFileName($url)
+    $downloadDirectoryPath = $env:TMP
+    $downloadFilePath = [System.IO.Path]::Combine($downloadDirectoryPath, $downloadFileName)
+
+    # Downloading
+    Invoke-WebRequest -Uri:$url -OutFile:$downloadFilePath
+
+    # Installation variables
+    $silentInstallArgs = @("/i", "`"$downloadFilePath`"", "/qn", "/norestart")
+
+    # Installing KeePassXc
+    $process = [System.Diagnostics.Process]::new()
+    $process.StartInfo.FileName = [System.IO.Path]::Combine($env:windir, "System32", "msiexec.exe")
+    $process.StartInfo.Arguments = $silentInstallArgs -join " "
+    $process.StartInfo.UseShellExecute = $true
+    $process.Start()
+    $process.WaitForExit()
+}
+
+else {
+    # Download Variables
+    $url = if ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "amd64") {
+        "https://github.com/keepassxreboot/keepassxc/releases/download/2.7.4/KeePassXC-2.7.4-Win64.zip"
+    }
+    elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "x86") {
+        "https://github.com/keepassxreboot/keepassxc/releases/download/2.6.6/KeePassXC-2.6.6-Win32-portable.zip"
+    }
+    else {
+        throw "Invalid Architecture"
+    }
+    $downloadFileName = [System.IO.Path]::GetFileName($url)
+    $downloadDirectoryPath = $env:TMP
+    $downloadFilePath = [System.IO.Path]::Combine($downloadDirectoryPath, $downloadFileName)
+
+    # Downloading
+    Invoke-WebRequest -Uri:$url -OutFile:$downloadFilePath
+
+    # Installation Varibles
+    $installationDirectoryPath = [System.IO.Path]::Combine($env:LOCALAPPDATA, "Programs", "KeePassXC")
+
+    # Creating Installation Directory
+    New-Item -Path:$installationDirectoryPath -ItemType Directory -ErrorAction SilentlyContinue
+
+    # Extracting KeePassXc
+    Expand-Archive -Path:$downloadFilePath -DestinationPath:$installationDirectoryPath
+
+    # Shortcut Variables
+    $executablePath = [System.IO.Path]::Combine((Get-ChildItem -Path:$installationDirectoryPath -Directory | Where-Object { $_.BaseName.Contains("KeePassXC") })[0].FullName, "KeePassXC.exe")
+    $shortcutDestinationPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft", "Windows", "Start Menu", "Programs", "KeePassXC.lnk")
+
+    # Crating Shortcut
+    $WScriptShell = New-Object -ComObject "WScript.Shell"
+    $shortcut = $WScriptShell.CreateShortcut($ShortcutDestinationPath)
+    $shortcut.TargetPath = $executablePath
+    $shortcut.Save()
+}
--- Menu/PortableApps/Principal.ps1
+++ Menu/PortableApps/Principal.ps1
@@ -80,7 +80,7 @@ while ($opcao -ne 0) {
     }
 
     elseif ($opcao -eq 12) {
-        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))), "PortableApps", "Install-KeePassXc.ps1"))
+        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))), "Install", "Install-KeePassXc.ps1"))
     }
 
     elseif ($opcao -eq 13) {
