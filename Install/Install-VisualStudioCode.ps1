c07b0ff9df2b9dc78ab63b812eb7f6d70f6426ff
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-06-24 15:54:10 -0300

    Making it easier to change url version

--- Install/Install-VisualStudioCode.ps1
+++ Install/Install-VisualStudioCode.ps1
@@ -1,16 +1,18 @@
 ﻿Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "functions", "Util", "Test-AdministratorPrivileges.ps1"))
 
 # Download Variables
+$baseUrl = "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/"
+$version = "1.74.3"
 $url = if (Test-AdministratorPrivileges) {
 
     if ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "amd64") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeSetup-x64-1.74.3.exe"
+        "$($baseUrl)VSCodeSetup-x64-$($version).exe"
     }
     elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "x86") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeSetup-ia32-1.74.3.exe"
+        "$($baseUrl)VSCodeSetup-ia32-$($version).exe"
     }
     elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "arm64") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeSetup-arm64-1.74.3.exe"
+        "$($baseUrl)VSCodeSetup-arm64-$($version).exe"
     }
     else {
         throw "Invalid Architecture"
@@ -19,13 +21,13 @@ $url = if (Test-AdministratorPrivileges) {
 else {
     
     if ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "amd64") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeUserSetup-x64-1.74.3.exe"
+        "$($baseUrl)VSCodeUserSetup-x64-$($version).exe"
     }
     elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "x86") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeUserSetup-ia32-1.74.3.exe"
+        "$($baseUrl)VSCodeUserSetup-ia32-$($version).exe"
     }
     elseif ($env:PROCESSOR_ARCHITECTURE.ToLower() -eq "arm64") {
-        "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/VSCodeUserSetup-arm64-1.74.3.exe"
+        "$($baseUrl)VSCodeUserSetup-arm64-$($version).exe"
     }
     else {
         throw "Invalid Architecture"
