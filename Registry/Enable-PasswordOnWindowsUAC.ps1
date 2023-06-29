eaaeb0eb58a529564406a5a0132d0b60b53531e8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-06-29 15:07:23 -0300

    Converting functions 'Enable-DualBootTimeSync', 'Enable-PasswordOnWindowsUAC' and 'Enable-RetpolineSpectreMitigation' to scripts.

---
+++ Registry/Enable-PasswordOnWindowsUAC.ps1
@@ -0,0 +1,12 @@
+﻿# https://superuser.com/questions/1085680/windows-10-make-uac-always-require-password
+
+param(
+    [Parameter(Mandatory = $false)] [System.Management.Automation.SwitchParameter]$RestoreDefault
+)
+
+if ($RestoreDefault) {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 5 -Force
+}
+else {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 1 -Force
+}
