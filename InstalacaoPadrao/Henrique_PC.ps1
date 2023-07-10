fe860480e0afc356f20c405d7969fffe3f3e35d6
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-07-10 11:54:31 -0300

    - Alterando os programas que serão instalados. - Removendo a instalação de pacotes do Chocolatey.

--- InstalacaoPadrao/Henrique_PC.ps1
+++ InstalacaoPadrao/Henrique_PC.ps1
@@ -11,24 +11,30 @@ Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryNam
 # Instalando o modulo package management
 Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery
 
-# Instalando o gerenciador de pacotes chocolatey
+# Instalando gerenciadores de pacotes
+## Instalando o gerenciador de pacotes chocolatey
 Install-Chocolatey
-
-# Instalando pacotes chocolatey
-choco install googlechrome firefox 7zip vlc vscode gsudo audacity gimp ccleaner cdburnerxp filezilla gh keepassxc libreoffice-still nomacs paint.net --yes
-choco install virtualbox --params "/ExtensionPack" --yes
-
-# Instalando o Winget
+## Instalando o Winget
 & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Winget.ps1"))
 
-# Instalando Python
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Python.ps1"))
 
-# Instalando o KeePass
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-KeePass.ps1"))
+# Instalando programas de produtividade
+## Instalando o Firefox
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-FirefoxEsr.ps1"))
+## Instalando o KeePassXc
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-KeePassXc.ps1"))
+## Instalando o LibreOffice
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-LibreOffice.ps1"))
 
-# Instalando o Git
+# Instalando ferramentas de desenvolvimento
+## Instalando o Git
 & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Git.ps1"))
+## Instalando o Visual Studio Code
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-VisualStudioCode.ps1"))
+## Instalando o IntelliJ
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-IntellijIdeaCommunity.ps1"))
+## Instalando Python
+& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Python.ps1"))
 
 # Adicionando Cygwin ao path
 Add-CygwinToPath
