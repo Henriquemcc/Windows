75cdb477fcad5d121bea6c94ebaaf4092377c7d4
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-22 18:46:45 -0300

    Reescrevendo script de instalação padrão

--- InstalacaoPadrao/Henrique_PC.ps1
+++ InstalacaoPadrao/Henrique_PC.ps1
@@ -1,43 +1,36 @@
-﻿Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "functions", "Chocolatey", "Chocolatey.psm1"))
-Import-Module -Name ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "functions", "Cygwin", "Cygwin.psm1"))
-
-# Configurando registro
+﻿# Configurando registro
 & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Registry", "Enable-PasswordOnWindowsUAC.ps1"))
 & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Registry", "Enable-RetpolineSpectreMitigation.ps1"))
 
-# Alterando o protocolo de seguranca
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+# Instalando Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Instalando o modulo package management
-Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery
+# Instalando Winget
+& ([System.IO.Path]::Combine($env:ProgramData, "chocolatey", "bin", "choco.exe")) install winget -y
 
-# Instalando gerenciadores de pacotes
-## Instalando o gerenciador de pacotes chocolatey
-Install-Chocolatey
-## Instalando o Winget
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Winget.ps1"))
+# Instalando programas
+winget install --silent --id Google.Chrome --source winget
+winget install --silent --id VideoLAN.VLC --source winget
+winget install --silent --id 7zip.7zip --source winget
+winget install --silent --id Microsoft.Teams --source winget --scope user
+winget install --silent --id Bitwarden.Bitwarden --source winget --scope user
+winget install --silent --id Oracle.JavaRuntimeEnvironment --source winget
+winget install --silent --id Mozilla.Firefox.ESR --source winget --scope user
+winget install --silent --id TheDocumentFoundation.LibreOffice.LTS --source winget
+winget install --silent --id DominikReichl.KeePass --source winget --scope user
+winget install --silent --id Microsoft.WindowsTerminal --source winget
+winget install --silent --id Microsoft.VisualStudioCode --source winget --scope user
+winget install --silent --id Git.Git -e --source winget --custom "/WindowsTerminalProfile" --scope user
+winget install --silent --id Python.Python.3 --source winget --scope user
+winget install --silent --id Oracle.JDK.21 --source winget
+winget install --silent --id EpicGames.EpicGamesLauncher --source winget
+winget install --silent --id Ubisoft.Connect --source winget
+winget install --silent --id WireGuard.WireGuard --source winget
+winget install --silent --id dotPDNLLC.paintdotnet --source winget --scope user
+winget install --silent --id Adobe.Acrobat.Reader.64-bit --source winget
+winget install --silent --id Valve.Steam --source winget
+winget install --silent --id ElectronicArts.EADesktop --source winget
+winget install --silent --id Rufus.Rufus --source winget --scope user
 
-
-# Instalando programas de produtividade
-## Instalando o Firefox
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-FirefoxEsr.ps1"))
-## Instalando o KeePassXc
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-KeePassXc.ps1"))
-## Instalando o LibreOffice
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-LibreOffice.ps1"))
-
-# Instalando ferramentas de desenvolvimento
-## Instalando o Git
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Git.ps1"))
-## Instalando o Visual Studio Code
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-VisualStudioCode.ps1"))
-## Instalando o IntelliJ
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-IntellijIdeaCommunity.ps1"))
-## Instalando Python
-& ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "Install", "Install-Python.ps1"))
-
-# Adicionando Cygwin ao path
-Add-CygwinToPath
-
-# Atualizando o menu de ajuda do PowerShell
-Update-Help -Force -ErrorAction Ignore
+# Atualizando programas
+winget upgrade --silent --all
