bb73258a4bba57f5b266c50c72a0c56242577626
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-03-07 14:53:26 -0300

    - Atualizando os programas antes de instalar novos programas. - Alterando a ordem da intalação dos programas para priorizar a instalação daqueles que requerem permissão de administrador. - Adicionando a instalação do Visual Studio Community, IntelliJ, sudo, BitDefender, Nvidia GeForce Experience, Intel Drive and Support Assistant. - Criando a variável $winget com a localização do winget.

--- InstalacaoPadrao/Henrique_PC.ps1
+++ InstalacaoPadrao/Henrique_PC.ps1
@@ -5,34 +5,45 @@
 # Instalando Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Instalando Winget
-& ([System.IO.Path]::Combine($env:ProgramData, "chocolatey", "bin", "choco.exe")) install winget -y
-
-# Instalando programas
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Google.Chrome --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id VideoLAN.VLC --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id 7zip.7zip --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Microsoft.Teams --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Bitwarden.Bitwarden --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Google.GoogleDrive --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Oracle.JavaRuntimeEnvironment --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Mozilla.Firefox.ESR --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id TheDocumentFoundation.LibreOffice.LTS --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id DominikReichl.KeePass --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Microsoft.WindowsTerminal --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Microsoft.VisualStudioCode --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Git.Git -e --source winget --custom "/WindowsTerminalProfile" --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Python.Python.3 --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Oracle.JDK.21 --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id EpicGames.EpicGamesLauncher --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Ubisoft.Connect --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id WireGuard.WireGuard --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id dotPDNLLC.paintdotnet --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Adobe.Acrobat.Reader.64-bit --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Valve.Steam --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id ElectronicArts.EADesktop --source winget
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Rufus.Rufus --source winget --scope user
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  install --silent --id Microsoft.BingWallpaper --source winget --scope user
+# Obtendo a localização do Winget
+$winget = (Get-Command -Name winget).Source
+if ($null -eq $winget) {
+    $winget = ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))
+}
 
 # Atualizando programas
-& ([System.IO.Path]::Combine($env:LOCALAPPDATA, "Microsoft", "WindowsApps", "winget.exe"))  upgrade --silent --all
+& $winget upgrade --silent --all
+
+# Instalando programas como administrador
+& $winget install --silent --id 7zip.7zip --source winget
+& $winget install --silent --id VideoLAN.VLC --source winget
+& $winget install --silent --id Oracle.JDK.21 --source winget
+& $winget install --silent --id JetBrains.IntelliJIDEA.Community --source winget
+& $winget install --silent --id Oracle.JavaRuntimeEnvironment --source winget
+& $winget install --silent --id Microsoft.WindowsTerminal --source winget
+& $winget install --silent --id Microsoft.VisualStudio.2022.Community --source winget
+& $winget install --silent --id TheDocumentFoundation.LibreOffice.LTS --source winget
+& $winget install --silent --id Google.GoogleDrive --source winget
+& $winget install --silent --id Adobe.Acrobat.Reader.64-bit --source winget
+& $winget install --silent --id Nvidia.GeForceExperience --source winget
+& $winget install --silent --id Intel.IntelDriverAndSupportAssistant --source winget
+& $winget install --silent --id Valve.Steam --source winget
+& $winget install --silent --id ElectronicArts.EADesktop --source winget
+& $winget install --silent --id EpicGames.EpicGamesLauncher --source winget
+& $winget install --silent --id Ubisoft.Connect --source winget
+& $winget install --silent --id WireGuard.WireGuard --source winget
+& $winget install --silent --id Bitdefender.Bitdefender --source winget
+
+# Instalando programas como usuário padrão
+& $winget install --silent --id Google.Chrome --source winget --scope user
+& $winget install --silent --id Microsoft.Teams --source winget --scope user
+& $winget install --silent --id Bitwarden.Bitwarden --source winget --scope user
+& $winget install --silent --id Mozilla.Firefox.ESR --source winget --scope user
+& $winget install --silent --id DominikReichl.KeePass --source winget --scope user
+& $winget install --silent --id Microsoft.VisualStudioCode --source winget --scope user
+& $winget install --silent --id Git.Git -e --source winget --custom "/WindowsTerminalProfile" --scope user
+& $winget install --silent --id Python.Python.3 --source winget --scope user
+& $winget install --silent --id dotPDNLLC.paintdotnet --source winget --scope user
+& $winget install --silent --id Rufus.Rufus --source winget --scope user
+& $winget install --silent --id Microsoft.BingWallpaper --source winget --scope user
+& $winget install --silent --id gerardog.gsudo --source winget --scope user
