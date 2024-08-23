a68c6793f01dccd5fa78cf2e13049d64bea3c8a4
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-08-23 15:24:02 -0300

    Adicionando script para montar disco no wsl

---
+++ wsl/MontarDisco.ps1
@@ -0,0 +1,26 @@
+# Obtendo a lista dos discos do Windows
+$discos = GET-CimInstance -query "SELECT * from Win32_DiskDrive"
+
+# Exibindo a lista dos discos
+for ($i = 0; $i -lt $discos.Count; $i++) {
+    Write-Host -Object "$i - $($discos[$i])"
+}
+
+# Obtendo o número do disco
+$numeroDisco = Read-Host -Prompt "Digite o número do disco que deseja montar: "
+try {
+    $numeroDisco = [int]$numeroDisco
+} catch {
+    Write-Host -Object "Número inválido"
+    exit
+}
+if ($numeroDisco -lt 0 -or $numeroDisco -ge $discos.Count) {
+    Write-Host -Object "Número inválido"
+    exit
+}
+
+# Obtendo a confirmação do usuário
+$confirmacao = Read-Host -Prompt "Tem certeza que deseja montar o disco $($discos[$numeroDisco])? (s/n)"
+if ($confirmacao.ToLower()[0] -eq "s") {
+    wsl --mount $($discos[$numeroDisco].DeviceID)
+}
