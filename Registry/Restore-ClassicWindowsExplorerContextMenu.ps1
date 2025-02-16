c709323d7737187ae3c0ba99cf797d3782bc3c0c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-02-16 17:39:46 -0300

    Adding script

---
+++ Registry/Restore-ClassicWindowsExplorerContextMenu.ps1
@@ -0,0 +1,13 @@
+﻿# https://www.howtogeek.com/759449/how-to-get-full-context-menus-in-windows-11s-file-explorer/
+
+param(
+    [Parameter(Mandatory = $false)][System.Management.Automation.SwitchParameter]$RestoreDefault
+)
+
+if ($RestoreDefault) {
+    Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force
+    Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force
+} else {
+    New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(default)" -Value "" -Force
+}
