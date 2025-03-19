8e889a494c8f426931e1c4d06b27f1aee262dfe2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-03-19 20:27:20 -0300

    Adicionando script (#26)

---
+++ ConfigurarGit.ps1
@@ -0,0 +1,5 @@
+﻿# https://stackoverflow.com/questions/52699177/how-to-fix-filename-too-long-error-during-git-clone
+& git config --global core.longpaths true
+
+# https://stackoverflow.com/questions/5787937/git-status-shows-files-as-changed-even-though-contents-are-the-same#:~:text=ignore%20them%20by-,git%20config%20core.filemode%20false,-Share
+& git config --global core.filemode false
