e19b3744f0f80f6ec7f40dec92a5284b75a8c27a
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-28 17:25:13 -0300

    Adicionando opções para instalar o Henrique-Desktop e Henrique-Laptop

--- Menu/InstalacaoPadrao.ps1
+++ Menu/InstalacaoPadrao.ps1
@@ -3,10 +3,11 @@
 function ObterOpcao {
 
     $_opcao = -1
-    while ($_opcao -lt 0 -or $_opcao -gt 1) {
+    while ($_opcao -lt 0 -or $_opcao -gt 2) {
         Write-Host -Object "O que deseja fazer?"
         Write-Host -Object "0 - Sair"
-        Write-Host -Object "1 - Instalar programas Henrique"
+        Write-Host -Object "1 - Instalar programas Henrique-Desktop"
+        Write-Host -Object "2 - Instalar programas Henrique-Laptop"
         $_opcao = Read-Int32
     }
 
@@ -18,6 +19,8 @@ while ($opcao -ne 0) {
     $opcao = ObterOpcao
 
     if ($opcao -eq 1) {
-        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "InstalarHenrique_PC.ps1"))
+        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "InstalarHenriqueDesktop.ps1"))
+    } elseif ($opcao -eq 2) {
+        & ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "InstalarHenriqueLaptop.ps1"))
     }
 }
