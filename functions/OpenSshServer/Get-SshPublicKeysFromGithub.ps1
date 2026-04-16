f429801a1edadb001d07b46486e6bc4fd01df5e9
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-04-16 08:31:47 -0300

    Adding script

---
+++ functions/OpenSshServer/Get-SshPublicKeysFromGithub.ps1
@@ -0,0 +1,25 @@
+﻿function Get-SshPublicKeysFromGithub{
+    param(
+        [Parameter(Mandatory = $true, Position = 0)][System.String]$GitHubUserName
+    )
+
+    <#
+    .SYNOPSIS
+        Obtains SSSH Public Keys From a GitHub User.
+    .DESCRIPTION
+        Obtains SSSH Public Keys From a GitHub User.
+    .PARAMETER GitHubUserName
+        GitHub username to download its public keys.
+    #>
+
+    # Downloading Public Keys
+    $publicKeys = Invoke-WebRequest -Uri "https://github.com/${GITHUB_USERNAME}.keys"
+
+    # Saving Public Keys to administrators_authorized_keys file
+    $publicKeys = $publicKeys.Content
+    $publicKeysFilePath = [System.IO.Path]::Combine($env:ProgramData, "ssh", "administrators_authorized_keys")
+    $publicKeys | Out-File -FilePath $publicKeysFilePath -Append
+
+    # Restarting Open SSH Server service
+    Restart-Service -Name "sshd"
+}
