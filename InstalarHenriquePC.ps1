0482bd96f9bee81d07e49eb6c48b9eb00f9d6a69
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-08-29 18:30:33 -0300

    Removendo Clion

--- InstalarHenriquePC.ps1
+++ InstalarHenriquePC.ps1
@@ -36,7 +36,7 @@ $programasInstalarWinget = @('Mozilla.Firefox.ESR', 'Google.Chrome', 'Bitwarden.
 'ONLYOFFICE.DesktopEditors', 'TheDocumentFoundation.LibreOffice', 'Adobe.Acrobat.Reader.64-bit', 'Discord.Discord', 'GIMP.GIMP.3', 'Oracle.VirtualBox',
 'Giorgiotani.Peazip', 'WireGuard.WireGuard', 'Microsoft.BingWallpaper', 'Audacity.Audacity', 'gerardog.gsudo', 'Rclone.Rclone', 'GitHub.cli', 'Valve.Steam',
 'ElectronicArts.EADesktop', 'EpicGames.EpicGamesLauncher', 'Ubisoft.Connect', 'DominikReichl.KeePass', 'Rufus.Rufus', 'JGraph.Draw',
-'Oracle.MySQLWorkbench', 'Nvidia.CUDA', 'Nvidia.GeForceExperience', 'PuTTY.PuTTY', 'HARMAN.AdobeAIR', 'JetBrains.CLion', 'Docker.DockerDesktop',
+'Oracle.MySQLWorkbench', 'Nvidia.CUDA', 'Nvidia.GeForceExperience', 'PuTTY.PuTTY', 'HARMAN.AdobeAIR', 'Docker.DockerDesktop',
 'DominikReichl.KeePass ', 'Insecure.Nmap', 'OBSProject.OBSStudio', 'qBittorrent.qBittorrent', 'Python.Launcher', 'Cisco.Webex', 'Gyan.FFmpeg',
 'Postman.Postman', 'Stremio.Stremio', 'JetBrains.Toolbox', 'KDE.Kdenlive', 'yt-dlp.yt-dlp', 'Canonical.Ubuntu', 'Microsoft.PowerToys',
 'ImageMagick.ImageMagick', 'Microsoft.Sysinternals.Whois', 'Microsoft.Coreutils', 'TeamViewer.TeamViewer')
